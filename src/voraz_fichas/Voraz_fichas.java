80a9f738268d28f25e56f0b80891af481f0eb613
KAICES <kaicespiu@gmail.com>
2017-05-02 00:32:45 -0500

    falta hacer el voraz a la matriz de fichas

--- src/voraz_fichas/Voraz_fichas.java
+++ src/voraz_fichas/Voraz_fichas.java
@@ -24,7 +24,7 @@ public class Voraz_fichas {
         tablero = new double[F][3] ; 
         t.ingresar(tablero);
         t.escribir(tablero);
-
+        t.ordenar(tablero);
     }   
     public void ingresar (double tablero[][]) {
             
@@ -32,12 +32,13 @@ public class Voraz_fichas {
             double bendim = 0 , x = 0 , y = 0, dim, beneficio ;;
             String num;
                 for(i=0;i<tablero.length;i++){
-                    
-                       x = Integer.parseInt(JOptionPane.showInputDialog("Dimension x de la pieza"+i+":"));
-                       y = Integer.parseInt(JOptionPane.showInputDialog("Dimension x de la pieza"+i+":"));
+                        
+                       j=i+1; 
+                       x = Integer.parseInt(JOptionPane.showInputDialog("Dimension x de la pieza"+j+":"));
+                       y = Integer.parseInt(JOptionPane.showInputDialog("Dimension y de la pieza"+j+":"));
                        dim = x * y;
                        tablero[i][0]= dim ; 
-                       beneficio = Integer.parseInt(JOptionPane.showInputDialog("Beneficio de la pieza"+i+":"));
+                       beneficio = Integer.parseInt(JOptionPane.showInputDialog("Beneficio de la pieza"+j+":"));
                        tablero[i][1]= beneficio ;
                        bendim = beneficio/dim;   
                        tablero[i][2]= bendim ;
@@ -61,4 +62,60 @@ public class Voraz_fichas {
 
            JOptionPane.showMessageDialog(null,Salida,"MATRIZ DE FICHAS",JOptionPane.INFORMATION_MESSAGE);
         }
+public void ordenar( double mat[ ][ ]) { // ordena por metodo shor de menor a mayor.
+    
+        int col = 2 ;
+        // Verifico que la columna seleccionada se encuentre dentro del rango permitido
+        // La variable col me indica la columna a ordenar
+        if (col < 0 || col > 2) {
+            return;
+        }
+
+        double aux ;
+
+        // Recorro la columna selecciona
+        for (int i = 0; i < mat.length ; i++) {
+            for (int j = i + 1; j < mat.length; j++) {
+                // Verifico si el elemento en la posición [i][col] es mayor que el de la posición [j][col]
+                if (mat[i][col] > mat[j][col]) {
+                    // Recorro las filas seleccionadas (i, j) e intercambio los elementos
+                    // Declaro la variable k para controlar la posición (columnas) en la fila
+                    for (int k = 0; k < 3; k++) {
+                        // Intercambio los elementos de las filas seleccionadas columna por columna
+                        aux = mat[i][k];
+                        mat[i][k] = mat[j][k];
+                        mat[j][k] = aux;
+                    }
+                }
+            }
+        }
+        escribir(mat);
+    }  
+
+    public void voraz ( double matvoraz [][], double mat [][] ) { 
+        
+        double dimTablero = I * J ;
+        double auxVoraz, dimFicha ; 
+        int col = 2 ; 
+        for(int i=mat.length-1; i>=0; i--){
+            for (int j = mat[i].length-1; i < mat.length ; j--){
+             if  (  mat[i][col] >= mat [j][col] && dimTablero < dimFicha ){
+                 
+                        for (int k = 0; k < 3; k++) {
+                        // Intercambio los elementos de las filas seleccionadas columna por columna
+                        auxVoraz = mat[i][k];
+                        mat[i][k] = mat[j][k];
+                        mat[j][k] = auxVoraz;
+                    }
+             }
+                                  
+             }
+         
+        }
+        
+       
+    }
+    
+    
+    
 }    
